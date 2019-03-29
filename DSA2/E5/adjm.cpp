4d4376bec9066da0911c19c58d76911e6c1fb241
trail99 <anup.kodlekere@gmail.com>
2019-03-29 18:48:16 +0530

    changed counter condition

--- DSA2/E5/adjm.cpp
+++ DSA2/E5/adjm.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 struct Graph {
   private:
@@ -15,17 +16,11 @@ struct Graph {
         adjMatrix[i][j] = false;
     }
 }
-
-  int getVertices();
-  int getEdges();
   
   void connectVertices(int,int,bool);
   void showGraph(); 
 
 };
-
-int Graph :: getVertices() { return vertices; }
-int Graph :: getEdges() { return edges; }
 
 void Graph :: connectVertices(int i , int j, bool state = true) {
   adjMatrix[i][j] = true;
@@ -33,9 +28,8 @@ void Graph :: connectVertices(int i , int j, bool state = true) {
 }
 
 void Graph :: showGraph() {
-  //printf(" %d "); 
   for(int i = 0; i < vertices; i++) {
-  //  printf("%d: ",i);
+    printf("%d: ",i);
     for(int j = 0; j < vertices; j++) 
       printf("%d ",adjMatrix[i][j]);
     printf("\n");
@@ -43,20 +37,16 @@ void Graph :: showGraph() {
 }
 
 int main() {
-  // No third argument: Undirected
-  // true: directed both sides, false: directed only one side
-  int v = 0, e = 0, counter = 0, srcVertex = 0, desVertex = 0;  
-  int state = 0;
+  int v = 0, e = 0, srcVertex = 0, desVertex = 0, state = 0;  
   printf("\n Enter the number of vertices and edges of the graph:");
   scanf("%d%d",&v,&e);
   Graph g(v,e);
-  while(counter != (g.getVertices())) {
-    counter++;
+  for(int counter = 0; counter < ceil(e); counter++) {
     printf("\n Enter source vertex number (0-vertices): ");
     scanf("%d",&srcVertex);
     printf("\n Enter destination vertex number (0-vertices): ");
     scanf("%d",&desVertex);
-    printf("\n Enter true for bidirectional, false otherwise: ");
+    printf("\n Enter 1 for bidirectional, 0 otherwise: ");
     scanf("%d",&state);
     printf("\n");
     g.connectVertices(srcVertex,desVertex,(bool)state);
